ee11abed15e14fbb27d7b32f677c8c5afa014b26
aler9 <46489434+aler9@users.noreply.github.com>
2023-04-01 12:03:37 +0200

    remove mutex from codecs.H264 and codecs.H265

--- pkg/codecs/h265.go
+++ pkg/codecs/h265.go
@@ -1,56 +1,8 @@
 package codecs
 
-import (
-	"sync"
-)
-
 // H265 is a H265 codec.
 type H265 struct {
 	VPS []byte
 	SPS []byte
 	PPS []byte
-
-	mutex sync.RWMutex
-}
-
-// SafeVPS returns the VPS.
-func (t *H265) SafeVPS() []byte {
-	t.mutex.RLock()
-	defer t.mutex.RUnlock()
-	return t.VPS
-}
-
-// SafeSPS returns the SPS.
-func (t *H265) SafeSPS() []byte {
-	t.mutex.RLock()
-	defer t.mutex.RUnlock()
-	return t.SPS
-}
-
-// SafePPS returns the PPS.
-func (t *H265) SafePPS() []byte {
-	t.mutex.RLock()
-	defer t.mutex.RUnlock()
-	return t.PPS
-}
-
-// SafeSetVPS sets the VPS.
-func (t *H265) SafeSetVPS(v []byte) {
-	t.mutex.Lock()
-	defer t.mutex.Unlock()
-	t.VPS = v
-}
-
-// SafeSetSPS sets the SPS.
-func (t *H265) SafeSetSPS(v []byte) {
-	t.mutex.Lock()
-	defer t.mutex.Unlock()
-	t.SPS = v
-}
-
-// SafeSetPPS sets the PPS.
-func (t *H265) SafeSetPPS(v []byte) {
-	t.mutex.Lock()
-	defer t.mutex.Unlock()
-	t.PPS = v
 }
