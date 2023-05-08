9776a664a83c398808b6eae352591116f4e94cfb
Alessandro Ros <aler9.dev@gmail.com>
2023-05-08 11:53:58 +0200

    add 32-bit tests (#12)

--- scripts/test.mk
+++ scripts/test.mk
@@ -1,10 +1,16 @@
+LBITS := $(shell getconf LONG_BIT)
+ifeq ($(LBITS),64)
+RACE=-race
+endif
+
 test-pkg:
-	go test -v -race -coverprofile=coverage-pkg.txt ./pkg/...
+	go test -v $(RACE) -coverprofile=coverage-pkg.txt ./pkg/...
 
 test-nodocker: test-pkg
 
 define DOCKERFILE_TEST
-FROM $(BASE_IMAGE)
+ARG ARCH
+FROM $$ARCH/$(BASE_IMAGE)
 RUN apk add --no-cache make git gcc musl-dev
 WORKDIR /s
 COPY go.mod go.sum ./
@@ -14,8 +20,15 @@ endef
 export DOCKERFILE_TEST
 
 test:
-	echo "$$DOCKERFILE_TEST" | docker build -q . -f - -t temp
+	echo "$$DOCKERFILE_TEST" | docker build -q . -f - -t temp --build-arg ARCH=amd64
 	docker run --rm -it \
+	--name temp \
+	temp \
+	make test-nodocker
+
+test32:
+	echo "$$DOCKERFILE_TEST" | docker build -q . -f - -t temp --build-arg ARCH=i386
+	docker run --rm \
 	--name temp \
 	temp \
 	make test-nodocker
