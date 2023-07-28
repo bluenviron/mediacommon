fa17f8422e548646e6e6f01ec0201e18b617cb4b
Alessandro Ros <aler9.dev@gmail.com>
2023-07-28 23:38:55 +0200

    mpegts: add Writer.WriteOpus() (#26)

--- pkg/formats/mpegts/opus_access_unit.go
+++ pkg/formats/mpegts/opus_access_unit.go
@@ -12,15 +12,34 @@ type opusAccessUnit struct {
 func (au *opusAccessUnit) unmarshal(buf []byte) (int, error) {
 	n, err := au.ControlHeader.unmarshal(buf)
 	if err != nil {
-		return 0, fmt.Errorf("could not decode Opus control header: %v", err)
+		return 0, fmt.Errorf("could not decode control header: %v", err)
 	}
 	buf = buf[n:]
 
-	if len(buf) < int(au.ControlHeader.PayloadSize) {
+	if len(buf) < au.ControlHeader.PayloadSize {
 		return 0, fmt.Errorf("buffer is too small")
 	}
 
 	au.Packet = buf[:au.ControlHeader.PayloadSize]
 
-	return n + int(au.ControlHeader.PayloadSize), nil
+	return n + au.ControlHeader.PayloadSize, nil
+}
+
+func (au *opusAccessUnit) marshalSize() int {
+	return au.ControlHeader.marshalSize() + au.ControlHeader.PayloadSize
+}
+
+func (au *opusAccessUnit) marshalTo(buf []byte) (int, error) {
+	if au.ControlHeader.PayloadSize != len(au.Packet) {
+		return 0, fmt.Errorf("invalid packet")
+	}
+
+	n, err := au.ControlHeader.marshalTo(buf)
+	if err != nil {
+		return 0, err
+	}
+
+	n += copy(buf[n:], au.Packet)
+
+	return n, nil
 }
--- pkg/formats/mpegts/opus_control_header.go
+++ pkg/formats/mpegts/opus_control_header.go
@@ -6,10 +6,44 @@ import (
 	"github.com/bluenviron/mediacommon/pkg/bits"
 )
 
+func unmarshalPayloadSize(buf []byte, pos *int) (int, error) {
+	res := 0
+
+	for {
+		next, err := bits.ReadBits(buf, pos, 8)
+		if err != nil {
+			return 0, err
+		}
+
+		res += int(next)
+
+		if next != 255 {
+			break
+		}
+	}
+
+	return res, nil
+}
+
+func marshalPayloadSizeSize(v int) int {
+	return v/255 + 1
+}
+
+func marshalPayloadSize(v int, ss int, buf []byte) {
+	for i := 0; i < (ss - 1); i++ {
+		buf[i] = 255
+	}
+	buf[ss-1] = byte(v % 255)
+}
+
 type opusControlHeader struct {
-	PayloadSize uint64
-	StartTrim   uint16
-	EndTrim     uint16
+	PayloadSize            int
+	StartTrimFlag          bool
+	EndTrimFlag            bool
+	ControlExtensionFlag   bool
+	StartTrim              uint16
+	EndTrim                uint16
+	ControlExtensionLength uint8
 }
 
 func (h *opusControlHeader) unmarshal(buf []byte) (int, error) {
@@ -25,28 +59,18 @@ func (h *opusControlHeader) unmarshal(buf []byte) (int, error) {
 		return 0, fmt.Errorf("invalid prefix")
 	}
 
-	startTrimFlag := bits.ReadFlagUnsafe(buf, &pos)
-	endTrimFlag := bits.ReadFlagUnsafe(buf, &pos)
-	controlExtensionFlag := bits.ReadFlagUnsafe(buf, &pos)
+	h.StartTrimFlag = bits.ReadFlagUnsafe(buf, &pos)
+	h.EndTrimFlag = bits.ReadFlagUnsafe(buf, &pos)
+	h.ControlExtensionFlag = bits.ReadFlagUnsafe(buf, &pos)
 
 	pos += 2 // reserved
 
-	h.PayloadSize = 0
-
-	for {
-		next, err := bits.ReadBits(buf, &pos, 8)
-		if err != nil {
-			return 0, err
-		}
-
-		h.PayloadSize += next
-
-		if next != 0xFF {
-			break
-		}
+	h.PayloadSize, err = unmarshalPayloadSize(buf, &pos)
+	if err != nil {
+		return 0, err
 	}
 
-	if startTrimFlag {
+	if h.StartTrimFlag {
 		err := bits.HasSpace(buf, pos, 16)
 		if err != nil {
 			return 0, err
@@ -56,23 +80,24 @@ func (h *opusControlHeader) unmarshal(buf []byte) (int, error) {
 		h.StartTrim = uint16(bits.ReadBitsUnsafe(buf, &pos, 13))
 	}
 
-	if endTrimFlag {
+	if h.EndTrimFlag {
 		err := bits.HasSpace(buf, pos, 16)
 		if err != nil {
 			return 0, err
 		}
 
-		pos += 3 // reservedch.PayloadSize
+		pos += 3 // reserved
 		h.EndTrim = uint16(bits.ReadBitsUnsafe(buf, &pos, 13))
 	}
 
-	if controlExtensionFlag {
-		le, err := bits.ReadBits(buf, &pos, 8)
+	if h.ControlExtensionFlag {
+		tmp, err := bits.ReadBits(buf, &pos, 8)
 		if err != nil {
 			return 0, err
 		}
+		h.ControlExtensionLength = uint8(tmp)
 
-		space := int(8 * le)
+		space := 8 * int(h.ControlExtensionLength)
 		err = bits.HasSpace(buf, pos, space)
 		if err != nil {
 			return 0, err
@@ -82,4 +107,58 @@ func (h *opusControlHeader) unmarshal(buf []byte) (int, error) {
 	}
 
 	return pos / 8, nil
+}
+
+func (h *opusControlHeader) marshalSize() int {
+	n := 2 + marshalPayloadSizeSize(h.PayloadSize)
+	if h.StartTrimFlag {
+		n += 2
+	}
+	if h.EndTrimFlag {
+		n += 2
+	}
+	if h.ControlExtensionFlag {
+		n++
+		n += int(h.ControlExtensionLength)
+	}
+	return n
+}
+
+func (h *opusControlHeader) marshalTo(buf []byte) (int, error) {
+	buf[0] = 0b01111111
+
+	buf[1] = 0b111 << 5
+	if h.StartTrimFlag {
+		buf[1] |= 1 << 4
+	}
+	if h.EndTrimFlag {
+		buf[1] |= 1 << 3
+	}
+	if h.ControlExtensionFlag {
+		buf[1] |= 1 << 2
+	}
+
+	ss := marshalPayloadSizeSize(h.PayloadSize)
+	marshalPayloadSize(h.PayloadSize, ss, buf[2:])
+	pos := 2 + ss
+
+	if h.StartTrimFlag {
+		buf[pos] = uint8(h.StartTrim >> 8)
+		buf[pos+1] = uint8(h.StartTrim)
+		pos += 2
+	}
+
+	if h.EndTrimFlag {
+		buf[pos] = uint8(h.EndTrim >> 8)
+		buf[pos+1] = uint8(h.EndTrim)
+		pos += 2
+	}
+
+	if h.ControlExtensionFlag {
+		buf[pos] = h.ControlExtensionLength
+		pos++
+		pos += int(h.ControlExtensionLength)
+	}
+
+	return pos, nil
 }
