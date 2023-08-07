ee62cfccc3c0a72311c7a372c5c0f6b8ef9036cd
aler9 <46489434+aler9@users.noreply.github.com>
2023-08-07 11:11:52 +0200

    add h265.IsRandomAccess()

--- pkg/codecs/av1/contains_key_frame_test.go
+++ pkg/codecs/av1/contains_key_frame_test.go
@@ -7,22 +7,16 @@ import (
 )
 
 func TestContainsKeyFrame(t *testing.T) {
-	for _, ca := range []struct {
-		name string
-		byts []byte
-	}{
-		{
-			"sequence header",
-			[]byte{
-				0x0a, 0x0e, 0x00, 0x00, 0x00, 0x4a, 0xab, 0xbf,
-				0xc3, 0x77, 0x6b, 0xe4, 0x40, 0x40, 0x40, 0x41,
-			},
-		},
-	} {
-		t.Run(ca.name, func(t *testing.T) {
-			ok, err := ContainsKeyFrame([][]byte{ca.byts})
-			require.NoError(t, err)
-			require.Equal(t, true, ok)
-		})
-	}
+	ok, err := ContainsKeyFrame([][]byte{{
+		0x0a, 0x0e, 0x00, 0x00, 0x00, 0x4a, 0xab, 0xbf,
+		0xc3, 0x77, 0x6b, 0xe4, 0x40, 0x40, 0x40, 0x41,
+	}})
+	require.NoError(t, err)
+	require.Equal(t, true, ok)
+
+	_, err = ContainsKeyFrame([][]byte{})
+	require.Error(t, err)
+
+	_, err = ContainsKeyFrame([][]byte{{}})
+	require.Error(t, err)
 }
