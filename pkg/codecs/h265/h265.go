e12612862e03b0e965d95bd668550a90cadc8886
database64128 <free122448@hotmail.com>
2023-08-12 23:27:25 +0800

    h264, h265: raise MaxAccessUnitSize to 8 MiB
    Fixes https://github.com/bluenviron/mediacommon/issues/35

--- pkg/codecs/h265/h265.go
+++ pkg/codecs/h265/h265.go
@@ -3,8 +3,8 @@ package h265
 
 const (
 	// MaxAccessUnitSize is the maximum size of an access unit.
-	// With a 50 Mbps 2160p60 H265 video, the maximum size does not seem to exceed 4 MiB.
-	MaxAccessUnitSize = 4 * 1024 * 1024
+	// With a 50 Mbps 2160p60 H265 video, the maximum size does not seem to exceed 8 MiB.
+	MaxAccessUnitSize = 8 * 1024 * 1024
 
 	// MaxNALUsPerAccessUnit is the maximum number of NALUs per access unit.
 	MaxNALUsPerAccessUnit = 20
