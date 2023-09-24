5354429162025ea3393cb9153266a31ac7b3cb06
aler9 <46489434+aler9@users.noreply.github.com>
2023-09-24 11:06:00 +0200

    h264: support empty NALUs inside AVCC
    (https://github.com/bluenviron/mediamtx/issues/2375)

--- pkg/codecs/h264/avcc.go
+++ pkg/codecs/h264/avcc.go
@@ -1,11 +1,15 @@
 package h264
 
 import (
+	"errors"
 	"fmt"
 )
 
+// ErrAVCCNoNALUs is returned by AVCCUnmarshal when no NALUs have been decoded.
+var ErrAVCCNoNALUs = errors.New("AVCC unit doesn't contain any NALU")
+
 // AVCCUnmarshal decodes an access unit from the AVCC stream format.
-// Specification: ?
+// Specification: ISO 14496-15, section 5.3.4.2.1
 func AVCCUnmarshal(buf []byte) ([][]byte, error) {
 	bl := len(buf)
 	pos := 0
@@ -21,31 +25,33 @@ func AVCCUnmarshal(buf []byte) ([][]byte, error) {
 		l := int(uint32(buf[pos])<<24 | uint32(buf[pos+1])<<16 | uint32(buf[pos+2])<<8 | uint32(buf[pos+3]))
 		pos += 4
 
-		if l == 0 {
-			return nil, fmt.Errorf("invalid NALU")
-		}
+		if l != 0 {
+			if (auSize + l) > MaxAccessUnitSize {
+				return nil, fmt.Errorf("access unit size (%d) is too big, maximum is %d", auSize+l, MaxAccessUnitSize)
+			}
 
-		if (auSize + l) > MaxAccessUnitSize {
-			return nil, fmt.Errorf("access unit size (%d) is too big, maximum is %d", auSize+l, MaxAccessUnitSize)
-		}
+			if (naluCount + 1) > MaxNALUsPerAccessUnit {
+				return nil, fmt.Errorf("NALU count (%d) exceeds maximum allowed (%d)",
+					len(ret)+1, MaxNALUsPerAccessUnit)
+			}
 
-		if (naluCount + 1) > MaxNALUsPerAccessUnit {
-			return nil, fmt.Errorf("NALU count (%d) exceeds maximum allowed (%d)",
-				len(ret)+1, MaxNALUsPerAccessUnit)
-		}
+			if (bl - pos) < l {
+				return nil, fmt.Errorf("invalid length")
+			}
 
-		if (bl - pos) < l {
-			return nil, fmt.Errorf("invalid length")
+			ret = append(ret, buf[pos:pos+l])
+			auSize += l
+			naluCount++
+			pos += l
 		}
-
-		ret = append(ret, buf[pos:pos+l])
-		auSize += l
-		naluCount++
-		pos += l
 
 		if (bl - pos) == 0 {
 			break
 		}
+	}
+
+	if ret == nil {
+		return nil, ErrAVCCNoNALUs
 	}
 
 	return ret, nil
@@ -60,7 +66,7 @@ func avccMarshalSize(au [][]byte) int {
 }
 
 // AVCCMarshal encodes an access unit into the AVCC stream format.
-// Specification: ?
+// Specification: ISO 14496-15, section 5.3.4.2.1
 func AVCCMarshal(au [][]byte) ([]byte, error) {
 	buf := make([]byte, avccMarshalSize(au))
 	pos := 0
