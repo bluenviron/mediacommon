2831f92fa26424df60cebfaf8259005030f5fd8a
Alessandro Ros <aler9.dev@gmail.com>
2024-10-20 16:17:25 +0200

    bits: rename WriteBitsUnsafe argument (#156)

--- pkg/bits/write.go
+++ pkg/bits/write.go
@@ -3,31 +3,31 @@ package bits
 // WriteBits writes N bits.
 //
 // Deprecated: replaced by WriteBitsUnsafe.
-func WriteBits(buf []byte, pos *int, bits uint64, n int) {
-	WriteBitsUnsafe(buf, pos, bits, n)
+func WriteBits(buf []byte, pos *int, v uint64, n int) {
+	WriteBitsUnsafe(buf, pos, v, n)
 }
 
 // WriteBitsUnsafe writes N bits.
-func WriteBitsUnsafe(buf []byte, pos *int, bits uint64, n int) {
+func WriteBitsUnsafe(buf []byte, pos *int, v uint64, n int) {
 	res := 8 - (*pos & 0x07)
 	if n < res {
-		buf[*pos>>0x03] |= byte(bits << (res - n))
+		buf[*pos>>0x03] |= byte(v << (res - n))
 		*pos += n
 		return
 	}
 
-	buf[*pos>>3] |= byte(bits >> (n - res))
+	buf[*pos>>3] |= byte(v >> (n - res))
 	*pos += res
 	n -= res
 
 	for n >= 8 {
-		buf[*pos>>3] = byte(bits >> (n - 8))
+		buf[*pos>>3] = byte(v >> (n - 8))
 		*pos += 8
 		n -= 8
 	}
 
 	if n > 0 {
-		buf[*pos>>3] = byte((bits & (1<<n - 1)) << (8 - n))
+		buf[*pos>>3] = byte((v & (1<<n - 1)) << (8 - n))
 		*pos += n
 	}
 }
