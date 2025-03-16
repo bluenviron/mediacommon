617d95076594c04d3762c1803ad8303082c6194c
Alessandro Ros <aler9.dev@gmail.com>
2025-03-16 13:57:10 +0100

    fmp4: replace NewPart* with Fill* (#185)

--- pkg/formats/fmp4/part_sample.go
+++ pkg/formats/fmp4/part_sample.go
@@ -15,44 +15,71 @@ type PartSample struct {
 }
 
 // NewPartSampleAV12 creates a sample with AV1 data.
+//
+// Deprecated: replaced by FillAV1.
 func NewPartSampleAV12(tu [][]byte) (*PartSample, error) {
-	bs, err := av1.Bitstream(tu).Marshal()
-	if err != nil {
-		return nil, err
-	}
-
-	return &PartSample{
-		IsNonSyncSample: !av1.IsRandomAccess2(tu),
-		Payload:         bs,
-	}, nil
+	ps := &PartSample{}
+	err := ps.FillAV1(tu)
+	return ps, err
 }
 
-// NewPartSampleH264 creates a sample with H264 data.
-func NewPartSampleH264(ptsOffset int32, au [][]byte) (*PartSample, error) {
-	avcc, err := h264.AVCC(au).Marshal()
+// FillAV1 fills a PartSample with AV1 data.
+func (ps *PartSample) FillAV1(tu [][]byte) error {
+	bs, err := av1.Bitstream(tu).Marshal()
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return &PartSample{
-		PTSOffset:       ptsOffset,
-		IsNonSyncSample: !h264.IsRandomAccess(au),
-		Payload:         avcc,
-	}, nil
+	ps.IsNonSyncSample = !av1.IsRandomAccess2(tu)
+	ps.Payload = bs
+
+	return nil
 }
 
 // NewPartSampleH265 creates a sample with H265 data.
+//
+// Deprecated: replaced by FillH265.
 func NewPartSampleH265(ptsOffset int32, au [][]byte) (*PartSample, error) {
+	ps := &PartSample{}
+	err := ps.FillH265(ptsOffset, au)
+	return ps, err
+}
+
+// FillH265 fills a PartSample with H265 data.
+func (ps *PartSample) FillH265(ptsOffset int32, au [][]byte) error {
 	avcc, err := h264.AVCC(au).Marshal()
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return &PartSample{
-		PTSOffset:       ptsOffset,
-		IsNonSyncSample: !h265.IsRandomAccess(au),
-		Payload:         avcc,
-	}, nil
+	ps.PTSOffset = ptsOffset
+	ps.IsNonSyncSample = !h265.IsRandomAccess(au)
+	ps.Payload = avcc
+
+	return nil
+}
+
+// NewPartSampleH264 creates a sample with H264 data.
+//
+// Deprecated: replaced by FillH264.
+func NewPartSampleH264(ptsOffset int32, au [][]byte) (*PartSample, error) {
+	ps := &PartSample{}
+	err := ps.FillH264(ptsOffset, au)
+	return ps, err
+}
+
+// FillH264 fills a PartSample with H264 data.
+func (ps *PartSample) FillH264(ptsOffset int32, au [][]byte) error {
+	avcc, err := h264.AVCC(au).Marshal()
+	if err != nil {
+		return err
+	}
+
+	ps.PTSOffset = ptsOffset
+	ps.IsNonSyncSample = !h264.IsRandomAccess(au)
+	ps.Payload = avcc
+
+	return nil
 }
 
 // GetAV1 gets AV1 data from the sample.
@@ -66,6 +93,11 @@ func (ps PartSample) GetAV1() ([][]byte, error) {
 	return tu, nil
 }
 
+// GetH265 gets H265 data from the sample.
+func (ps PartSample) GetH265() ([][]byte, error) {
+	return ps.GetH264()
+}
+
 // GetH264 gets H264 data from the sample.
 func (ps PartSample) GetH264() ([][]byte, error) {
 	var au h264.AVCC
@@ -75,9 +107,4 @@ func (ps PartSample) GetH264() ([][]byte, error) {
 	}
 
 	return au, nil
-}
-
-// GetH265 gets H265 data from the sample.
-func (ps PartSample) GetH265() ([][]byte, error) {
-	return ps.GetH264()
 }
