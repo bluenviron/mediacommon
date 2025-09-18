bluenviron/mediacommon

commit 66e4ac94eac6b1e17e82c9e8ed311a08a24f5dac

h265: fix DTS extractor with libx265 (#245)

pkg/codecs/h265/dts_extractor.go
@@ -1,6 +1,7 @@
 package h265
 
 import (
+	"bytes"
 	"fmt"
 	"math"
 
@@ -128,7 +129,9 @@ func getPTSDTSDiff(buf []byte, sps *SPS, pps *PPS) (int, error) {
 
 // DTSExtractor computes DTS from PTS.
 type DTSExtractor struct {
+	sps             []byte
 	spsp            *SPS
+	pps             []byte
 	ppsp            *PPS
 	prevDTSFilled   bool
 	prevDTS         int64
@@ -155,34 +158,36 @@ func (d *DTSExtractor) extractInner(au [][]byte, pts int64) (int64, error) {
 		typ := NALUType((nalu[0] >> 1) & 0b111111)
 		switch typ {
 		case NALUType_SPS_NUT:
-			var spsp SPS
-			err := spsp.Unmarshal(nalu)
-			if err != nil {
-				return 0, fmt.Errorf("invalid SPS: %w", err)
-			}
+			if !bytes.Equal(d.sps, nalu) {
+				var spsp SPS
+				err := spsp.Unmarshal(nalu)
+				if err != nil {
+					return 0, fmt.Errorf("invalid SPS: %w", err)
+				}
 
-			if spsp.VUI != nil && spsp.VUI.TimingInfo != nil && spsp.VUI.TimingInfo.TimeScale == 0 {
-				return 0, fmt.Errorf("invalid SPS VUI TimeScale")
-			}
+				d.spsp = &spsp
+				d.sps = nalu
 
-			d.spsp = &spsp
-
-			// reset state
-			d.prevDTSFilled = false
-			if len(d.spsp.MaxNumReorderPics) == 1 {
-				d.reorderedFrames = int(d.spsp.MaxNumReorderPics[0])
-			} else {
-				d.reorderedFrames = 0
+				// reset state
+				if len(d.spsp.MaxNumReorderPics) == 1 {
+					d.reorderedFrames = int(d.spsp.MaxNumReorderPics[0])
+				} else {
+					d.reorderedFrames = 0
+				}
+				d.pause = d.reorderedFrames
 			}
-			d.pause = d.reorderedFrames
 
 		case NALUType_PPS_NUT:
-			var ppsp PPS
-			err := ppsp.Unmarshal(nalu)
-			if err != nil {
-				return 0, fmt.Errorf("invalid PPS: %w", err)
+			if !bytes.Equal(d.pps, nalu) {
+				var ppsp PPS
+				err := ppsp.Unmarshal(nalu)
+				if err != nil {
+					return 0, fmt.Errorf("invalid PPS: %w", err)
+				}
+
+				d.ppsp = &ppsp
+				d.pps = nalu
 			}
-			d.ppsp = &ppsp
 
 		case NALUType_IDR_W_RADL, NALUType_IDR_N_LP:
 			idr = nalu
@@ -226,13 +231,6 @@ func (d *DTSExtractor) extractInner(au [][]byte, pts int64) (int64, error) {
 	if d.pause > 0 {
 		d.pause--
 		if !d.prevDTSFilled {
-			if d.spsp.VUI != nil && d.spsp.VUI.TimingInfo != nil {
-				timeDiff := int64(d.pause+1) * 90000 *
-					int64(d.spsp.VUI.TimingInfo.NumUnitsInTick) / int64(d.spsp.VUI.TimingInfo.TimeScale)
-				dts := pts - timeDiff
-				d.pause = 0
-				return dts, nil
-			}
 			return pts, nil
 		}
 		return d.prevDTS + 90, nil

pkg/codecs/h265/dts_extractor_test.go
@@ -45,7 +45,7 @@ var casesDTSExtractor = []struct {
 					},
 				},
 				0,
-				-6000,
+				0,
 			},
 			{
 				[][]byte{{ // TRAIL_R
@@ -54,7 +54,7 @@ var casesDTSExtractor = []struct {
 					0x54, 0x57, 0x4e, 0x0a,
 				}},
 				9000,
-				-3000,
+				90,
 			},
 			{
 				[][]byte{{ // TRAIL_R
@@ -63,7 +63,7 @@ var casesDTSExtractor = []struct {
 					0x26, 0x5f, 0x10, 0x9c,
 				}},
 				5999,
-				-1,
+				2059,
 			},
 			{
 				[][]byte{{ // TRAIL_N
@@ -132,7 +132,7 @@ var casesDTSExtractor = []struct {
 					},
 				},
 				100,
-				-4400,
+				100,
 			},
 			{
 				[][]byte{
@@ -143,7 +143,7 @@ var casesDTSExtractor = []struct {
 					},
 				},
 				6130,
-				865,
+				3115,
 			},
 			{
 				[][]byte{
@@ -154,7 +154,7 @@ var casesDTSExtractor = []struct {
 					},
 				},
 				9100,
-				4982,
+				6107,
 			},
 		},
 	},
@@ -199,7 +199,7 @@ var casesDTSExtractor = []struct {
 					},
 				},
 				182999,
-				179999,
+				182999,
 			},
 			{
 				[][]byte{
@@ -217,7 +217,7 @@ var casesDTSExtractor = []struct {
 					},
 				},
 				188999,
-				181499,
+				183089,
 			},
 			{
 				[][]byte{
@@ -235,7 +235,7 @@ var casesDTSExtractor = []struct {
 					},
 				},
 				185999,
-				182999,
+				184059,
 			},
 			{
 				[][]byte{
@@ -394,6 +394,103 @@ var casesDTSExtractor = []struct {
 				},
 				24000,
 				9000,
+			},
+		},
+	},
+	{
+		"libx265, bframes=4",
+		[]sequenceSample{
+			{
+				[][]byte{
+					{
+						0x40, 0x01, 0x0c, 0x01, 0xff, 0xff, 0x01, 0x60,
+						0x00, 0x00, 0x03, 0x00, 0x90, 0x00, 0x00, 0x03,
+						0x00, 0x00, 0x03, 0x00, 0x78, 0x95, 0x98, 0x09,
+					},
+					{
+						0x42, 0x01, 0x01, 0x01, 0x60, 0x00, 0x00, 0x03,
+						0x00, 0x90, 0x00, 0x00, 0x03, 0x00, 0x00, 0x03,
+						0x00, 0x78, 0xa0, 0x03, 0xc0, 0x80, 0x11, 0x07,
+						0xcb, 0x96, 0x56, 0x64, 0xa4, 0xc2, 0xf0, 0x16,
+						0x80, 0x80, 0x00, 0x00, 0x03, 0x00, 0x80, 0x00,
+						0x00, 0x0f, 0x04,
+					},
+					{
+						0x44, 0x01, 0xc0, 0x73, 0xc1, 0x89,
+					},
+					{
+						0x28, 0x01, 0xac, 0x64, 0x11, 0x0b, 0x35, 0x45,
+						0xb4, 0xa0, 0x95, 0x12, 0x90, 0x42, 0x48, 0xe1,
+						0x2e, 0x21, 0xc4, 0x7c, 0xd4, 0x95, 0x63, 0x44,
+						0x2f, 0x04, 0x98, 0x87, 0x03, 0xe2, 0xdc, 0x43,
+						0x09, 0x61, 0x4c, 0x6f, 0x4a, 0x39, 0x6e, 0x27,
+						0xb6, 0x50, 0x3b, 0x03, 0xd0, 0x45, 0x1f, 0x4c,
+						0xf1, 0x66,
+					},
+				},
+				0,
+				0,
+			},
+			{
+				[][]byte{
+					{
+						0x40, 0x01, 0x0c, 0x01, 0xff, 0xff, 0x01, 0x60,
+						0x00, 0x00, 0x03, 0x00, 0x90, 0x00, 0x00, 0x03,
+						0x00, 0x00, 0x03, 0x00, 0x78, 0x95, 0x98, 0x09,
+					},
+					{
+						0x42, 0x01, 0x01, 0x01, 0x60, 0x00, 0x00, 0x03,
+						0x00, 0x90, 0x00, 0x00, 0x03, 0x00, 0x00, 0x03,
+						0x00, 0x78, 0xa0, 0x03, 0xc0, 0x80, 0x11, 0x07,
+						0xcb, 0x96, 0x56, 0x64, 0xa4, 0xc2, 0xf0, 0x16,
+						0x80, 0x80, 0x00, 0x00, 0x03, 0x00, 0x80, 0x00,
+						0x00, 0x0f, 0x04,
+					},
+					{
+						0x44, 0x01, 0xc0, 0x73, 0xc1, 0x89,
+					},
+					{
+						0x2a, 0x01, 0xac, 0x14, 0xa5, 0x46, 0x01, 0x10,
+						0xb2, 0xca, 0x41, 0x47, 0x18, 0xd2, 0x20, 0x02,
+						0xec, 0x71, 0x8b, 0xd1, 0x58, 0x36, 0x88, 0xc9,
+						0x1d, 0x2d, 0x82, 0x80, 0x37, 0x05, 0x80, 0x46,
+						0x29, 0x43, 0x38, 0x71, 0x10, 0x05, 0x8c, 0x84,
+						0x10, 0x61, 0xcd, 0x15, 0xc4, 0x20, 0x2c, 0x02,
+						0x8e, 0x30,
+					},
+				},
+				15000,
+				90,
+			},
+			{
+				[][]byte{
+					{
+						0x12, 0x01, 0xe0, 0x64, 0x9d, 0x60, 0x81, 0x40,
+						0x22, 0x28, 0xc4, 0x21, 0x08, 0x42, 0x10, 0x84,
+						0x20, 0x84, 0x21, 0x08, 0x42, 0x10, 0x84, 0x21,
+						0x08, 0x78, 0x92, 0x0d, 0xca, 0x31, 0xa0, 0x92,
+						0x80, 0x27, 0x20, 0x00, 0x05, 0x04, 0x00, 0x09,
+						0x88, 0x00, 0x11, 0xf0, 0x00, 0x1d, 0xf0, 0x00,
+						0x31, 0x20,
+					},
+				},
+				9000,
+				2317,
+			},
+			{
+				[][]byte{
+					{
+						0x10, 0x01, 0xe0, 0x24, 0xf5, 0x5e, 0x89, 0x02,
+						0xd0, 0x44, 0x43, 0x22, 0x22, 0x22, 0x22, 0x21,
+						0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x76,
+						0x33, 0xa7, 0x22, 0x80, 0x83, 0x80, 0x3e, 0xe0,
+						0x00, 0x05, 0xfc, 0x00, 0x0b, 0x78, 0x00, 0x15,
+						0x10, 0x00, 0x24, 0xa0, 0x00, 0x36, 0xa0, 0x00,
+						0x54, 0xc0,
+					},
+				},
+				3000,
+				3000,
 			},
 		},
 	},

pkg/codecs/h264/dts_extractor.go
@@ -121,7 +121,6 @@ func (d *DTSExtractor) extractInner(au [][]byte, pts int64) (int64, bool, error)
 				d.spsp = &spsp
 
 				// reset state
-				d.prevDTSFilled = false
 				d.expectedPOC = 0
 				d.reorderedFrames = 0
 				d.pause = 0

commit dbeee4ef861d137d988af9633ba8739919ecb3e9

h265: fix DTS extractor regression after #245
IDR frames which are also B-frames are supported again.

pkg/codecs/h265/dts_extractor.go
@@ -156,6 +156,7 @@ func (d *DTSExtractor) extractInner(au [][]byte, pts int64) (int64, error) {
 
 	for _, nalu := range au {
 		typ := NALUType((nalu[0] >> 1) & 0b111111)
+
 		switch typ {
 		case NALUType_SPS_NUT:
 			if !bytes.Equal(d.sps, nalu) {
@@ -231,13 +232,27 @@ func (d *DTSExtractor) extractInner(au [][]byte, pts int64) (int64, error) {
 	if d.pause > 0 {
 		d.pause--
 		if !d.prevDTSFilled {
-			return pts, nil
+			var timeDiff int64
+			if d.spsp.VUI != nil && d.spsp.VUI.TimingInfo != nil && d.spsp.VUI.TimingInfo.TimeScale != 0 {
+				timeDiff = int64(ptsDTSDiff) * 90000 *
+					int64(d.spsp.VUI.TimingInfo.NumUnitsInTick) / int64(d.spsp.VUI.TimingInfo.TimeScale)
+			} else {
+				timeDiff = 9000
+			}
+			return pts - timeDiff, nil
 		}
 		return d.prevDTS + 90, nil
 	}
 
 	if !d.prevDTSFilled {
-		return pts, nil
+		var timeDiff int64
+		if d.spsp.VUI != nil && d.spsp.VUI.TimingInfo != nil && d.spsp.VUI.TimingInfo.TimeScale != 0 {
+			timeDiff = int64(ptsDTSDiff) * 90000 *
+				int64(d.spsp.VUI.TimingInfo.NumUnitsInTick) / int64(d.spsp.VUI.TimingInfo.TimeScale)
+		} else {
+			timeDiff = 9000
+		}
+		return pts - timeDiff, nil
 	}
 
 	return d.prevDTS + (pts-d.prevDTS)/(int64(ptsDTSDiff)+1), nil

pkg/codecs/h265/dts_extractor_test.go
@@ -45,7 +45,7 @@ var casesDTSExtractor = []struct {
 					},
 				},
 				0,
-				0,
+				-6000,
 			},
 			{
 				[][]byte{{ // TRAIL_R
@@ -54,7 +54,7 @@ var casesDTSExtractor = []struct {
 					0x54, 0x57, 0x4e, 0x0a,
 				}},
 				9000,
-				90,
+				-5910,
 			},
 			{
 				[][]byte{{ // TRAIL_R
@@ -63,7 +63,7 @@ var casesDTSExtractor = []struct {
 					0x26, 0x5f, 0x10, 0x9c,
 				}},
 				5999,
-				2059,
+				-1941,
 			},
 			{
 				[][]byte{{ // TRAIL_N
@@ -132,7 +132,7 @@ var casesDTSExtractor = []struct {
 					},
 				},
 				100,
-				100,
+				-4400,
 			},
 			{
 				[][]byte{
@@ -143,7 +143,7 @@ var casesDTSExtractor = []struct {
 					},
 				},
 				6130,
-				3115,
+				865,
 			},
 			{
 				[][]byte{
@@ -154,7 +154,7 @@ var casesDTSExtractor = []struct {
 					},
 				},
 				9100,
-				6107,
+				4982,
 			},
 		},
 	},
@@ -199,7 +199,7 @@ var casesDTSExtractor = []struct {
 					},
 				},
 				182999,
-				182999,
+				179999,
 			},
 			{
 				[][]byte{
@@ -217,7 +217,7 @@ var casesDTSExtractor = []struct {
 					},
 				},
 				188999,
-				183089,
+				180089,
 			},
 			{
 				[][]byte{
@@ -235,7 +235,7 @@ var casesDTSExtractor = []struct {
 					},
 				},
 				185999,
-				184059,
+				182059,
 			},
 			{
 				[][]byte{
@@ -293,7 +293,7 @@ var casesDTSExtractor = []struct {
 					},
 				},
 				0,
-				0,
+				-9000,
 			},
 			{
 				[][]byte{
@@ -313,7 +313,7 @@ var casesDTSExtractor = []struct {
 					},
 				},
 				12000,
-				90,
+				-8910,
 			},
 			{
 				[][]byte{
@@ -333,7 +333,7 @@ var casesDTSExtractor = []struct {
 					},
 				},
 				6000,
-				2060,
+				-3940,
 			},
 			{
 				[][]byte{
@@ -429,7 +429,7 @@ var casesDTSExtractor = []struct {
 					},
 				},
 				0,
-				0,
+				-6000,
 			},
 			{
 				[][]byte{
@@ -460,7 +460,7 @@ var casesDTSExtractor = []struct {
 					},
 				},
 				15000,
-				90,
+				-5910,
 			},
 			{
 				[][]byte{
@@ -475,7 +475,7 @@ var casesDTSExtractor = []struct {
 					},
 				},
 				9000,
-				2317,
+				-2183,
 			},
 			{
 				[][]byte{
@@ -491,6 +491,72 @@ var casesDTSExtractor = []struct {
 				},
 				3000,
 				3000,
+			},
+		},
+	},
+	{
+		"b-frame IDR",
+		[]sequenceSample{
+			{
+				[][]byte{
+					{
+						0x40, 0x01, 0x0c, 0x01, 0xff, 0xff, 0x01, 0x60,
+						0x00, 0x00, 0x03, 0x00, 0x90, 0x00, 0x00, 0x03,
+						0x00, 0x00, 0x03, 0x00, 0x78, 0x95, 0x98, 0x09,
+					},
+					{
+						0x42, 0x01, 0x01, 0x01, 0x60, 0x00, 0x00, 0x03,
+						0x00, 0x90, 0x00, 0x00, 0x03, 0x00, 0x00, 0x03,
+						0x00, 0x78, 0xa0, 0x03, 0xc0, 0x80, 0x32, 0x16,
+						0x59, 0x59, 0xa4, 0x93, 0x2b, 0xc0, 0x5a, 0x80,
+						0x80, 0x80, 0x82, 0x00, 0x00, 0x07, 0xd2, 0x00,
+						0x00, 0xbb, 0x80, 0x10,
+					},
+					{
+						0x44, 0x01, 0xc1, 0x72, 0xb4, 0x62, 0x40,
+					},
+					{
+						0x2a, 0x01, 0xae, 0x44, 0x59, 0x48, 0xc9, 0xd4,
+						0x68, 0xd1, 0x64, 0x7c, 0x76, 0x77, 0xce, 0x07,
+						0x54, 0xca, 0x8f, 0x8d, 0x90, 0x33, 0x64, 0x52,
+						0x7c, 0x48, 0x16, 0xc0, 0x9c, 0x80, 0xf8, 0x81,
+						0x5b, 0xff, 0xf8, 0x76, 0x7c, 0x92, 0xd6, 0xb7,
+						0xa8, 0x55, 0x9c, 0x20, 0xc0, 0x9b, 0x30, 0x11,
+						0xdb, 0x80,
+					},
+				},
+				2466180,
+				2443658,
+			},
+			{
+				[][]byte{
+					{
+						0x12, 0x01, 0xf1, 0xe2, 0x27, 0x52, 0xd7, 0xdc,
+						0x21, 0xa3, 0x45, 0x03, 0x0c, 0x14, 0x30, 0x41,
+						0x16, 0xe1, 0xa9, 0x46, 0x12, 0x06, 0xc1, 0xe0,
+						0x14, 0x03, 0xc0, 0x92, 0x00, 0x8c, 0xb0, 0x66,
+						0xc2, 0x1c, 0xf7, 0x66, 0xad, 0xf4, 0xcb, 0x6a,
+						0x81, 0x90, 0xcc, 0xe9, 0xac, 0xc8, 0xec, 0x8d,
+						0x3f, 0x30,
+					},
+				},
+				2458710,
+				2443748,
+			},
+			{
+				[][]byte{
+					{
+						0x10, 0x01, 0xf1, 0xa6, 0xf5, 0x55, 0xf4, 0x89,
+						0x04, 0x06, 0x89, 0x0e, 0xb7, 0xff, 0xa5, 0x33,
+						0xa8, 0x5c, 0xee, 0x18, 0x0b, 0x06, 0xc1, 0x20,
+						0x78, 0x91, 0x00, 0xce, 0xc0, 0xd7, 0x1c, 0x8d,
+						0x93, 0x89, 0x2c, 0xb3, 0x03, 0xe8, 0xd1, 0xc3,
+						0x4c, 0xe2, 0x9c, 0xee, 0xa9, 0xc2, 0x57, 0xc3,
+						0x1c, 0x68,
+					},
+				},
+				2451240,
+				2451240,
 			},
 		},
 	},